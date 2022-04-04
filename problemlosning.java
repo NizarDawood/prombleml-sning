f41df0d3bdc8a3dc1ecca3feb0080b27111be430
NizarDawood <77719056+NizarDawood@users.noreply.github.com>
2022-04-04 11:17:57 +0200

    Add files via upload

---
+++ problemlosning.java
@@ -0,0 +1,29 @@
+public class problemlosning {
+    public static void main(String[] args) {
+
+       String ord =  "hej";
+
+String förnamn = "Nizar";
+String efternamn = "dawood";
+System.out.println(efternamn+förnamn);
+
+         String s = "Hejsan svejsan!";
+        String r = revers(s.toCharArray());
+
+        int plånbok = 1;
+
+        plånbok + 1
+    }
+//String pal = "tenet";
+
+    private static char[] revers(char[] revers) {
+        private static String revers( String ) {
+
+        char[] ord = s.toCharArray();;
+        for (int i = ord.length - 1; i >= 0; i--) {
+
+        }
+        return ord;
+    }
+}
+
